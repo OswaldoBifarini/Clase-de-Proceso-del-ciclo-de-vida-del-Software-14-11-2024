beb8c71d07a4b0a3998408e3e1f67cf3e793fe71
OswaldoBifarini <jgbifarini@gmail.com>
2024-11-15 16:58:45 -0500

    Comentario 1

---
+++ src/main/java/ec/edu/espoc/procesociclovidasoftware/ProcesoCicloVidaSoftware.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoc.procesociclovidasoftware;
+
+/**
+ *
+ * @author DELL
+ */
+public class ProcesoCicloVidaSoftware {
+
+    public static void main(String[] args) {
+        System.out.println("Clase de 14/11/2024");
+        
+    }
+}
